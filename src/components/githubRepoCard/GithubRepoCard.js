9e54f8c46779d9cac32de3cb56492c96ba395744
Adesh Bhosale <adesh@Adeshs-MacBook-Air.local>
2024-05-20 18:01:52 -0400

    gitrepocard

--- src/components/githubRepoCard/GithubRepoCard.js
+++ src/components/githubRepoCard/GithubRepoCard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 import ProjectLanguages from "../../components/projectLanguages/ProjectLanguages";
 import "./GithubRepoCard.css";
